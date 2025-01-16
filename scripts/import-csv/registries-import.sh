ecbe3cf371161d561f63dc8e9c6ed21b13c2cde7
flaminiaScarciofolo <flaminia.scarciofolo@nttdata.com>
2025-01-16 10:01:38 +0100

    Added README to use script for import of radd registry csv

--- scripts/import-csv/registries-import.sh
+++ scripts/import-csv/registries-import.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-CSV_PATH="test.csv"                                       # Sostituire con il path del file CSV
-API_BASE_URL="https://api.radd.dev.notifichedigitali.it"  # sostituire con il base url per l'Api di import massivo di radd-alt
-JWT="JWT"                                                 # Sostituire con il token JWT
-CX_UID="UID"                                              # Sostituire con il valore del campo uid
+if [ "$#" -ne 4 ]; then
+    exit 1
+fi
+
+CSV_PATH="$1"                                       # Path del file CSV (passato come primo argomento)
+API_BASE_URL="$2"                                   # Base URL per l'API (passato come secondo argomento)
+JWT="$3"                                            # Token JWT (passato come terzo argomento)
+CX_UID="$4"                                         # UID (passato come quarto argomento)
 
 calculate_sha256() {
     local file=$1
@@ -16,9 +20,8 @@ if [ ! -f "$CSV_PATH" ]; then
 fi
 
 CHECKSUM=$(calculate_sha256 "$CSV_PATH")
-echo "Checksum calcolato: $CHECKSUM"
 
-RESPONSE=$(curl -v POST "$API_BASE_URL/radd-net/api/v1/registry/import/upload" \
+RESPONSE=$(curl POST "$API_BASE_URL/radd-net/api/v1/registry/import/upload" \
            -H "uid: $CX_UID" \
            -H "Authorization: Bearer $JWT" \
            -H "Content-Type: application/json" \
@@ -27,6 +30,7 @@ RESPONSE=$(curl -v POST "$API_BASE_URL/radd-net/api/v1/registry/import/upload" \
 
 URL=$(echo "$RESPONSE" | jq -r '.url')
 SECRET=$(echo "$RESPONSE" | jq -r '.secret')
+REQUEST_ID=$(echo "$RESPONSE" | jq -r '.requestId')
 
 if [ -z "$URL" ] || [ -z "$SECRET" ]; then
     echo "Errore: Risposta incompleta, URL o secret mancanti."
@@ -40,3 +44,4 @@ curl -X PUT "$URL" \
     --data-binary "@$CSV_PATH"
 
 echo "Richiesta di import massivo completata."
+echo "REQUEST_ID: $REQUEST_ID"
