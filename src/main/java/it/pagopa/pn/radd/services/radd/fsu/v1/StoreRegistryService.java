737f7a32a9f76b6a51d284e533a10a21dadcfc5a
michelescara <michele.scaramuzzino@dgsspa.com>
2025-07-24 11:50:04 +0200

    PN-15831: Aligned store locator API to new models. Fixed tests.

--- src/main/java/it/pagopa/pn/radd/services/radd/fsu/v1/StoreRegistryService.java
+++ src/main/java/it/pagopa/pn/radd/services/radd/fsu/v1/StoreRegistryService.java
@@ -1,7 +1,10 @@
 package it.pagopa.pn.radd.services.radd.fsu.v1;
 
 import it.pagopa.pn.radd.alt.generated.openapi.server.v1.dto.StoreRegistriesResponse;
+import it.pagopa.pn.radd.mapper.RaddRegistryMapper;
+import it.pagopa.pn.radd.mapper.StoreRegistriesMapper;
 import it.pagopa.pn.radd.middleware.db.RaddRegistryDAO;
+import it.pagopa.pn.radd.middleware.db.RaddRegistryV2DAO;
 import it.pagopa.pn.radd.utils.RaddRegistryUtils;
 import lombok.CustomLog;
 import lombok.RequiredArgsConstructor;
@@ -13,13 +16,13 @@ import reactor.core.publisher.Mono;
 @CustomLog
 public class StoreRegistryService {
 
-    private final RaddRegistryDAO raddRegistryDAO;
-    private final RaddRegistryUtils raddRegistryUtils;
+    private final RaddRegistryV2DAO raddRegistryDAO;
+    private final StoreRegistriesMapper mapper;
 
 
     public Mono<StoreRegistriesResponse> retrieveStoreRegistries(Integer limit, String lastKey) {
         return raddRegistryDAO.scanRegistries(limit, lastKey)
-                .map(raddRegistryUtils::mapToStoreRegistriesResponse);
+                .map(mapper::mapToStoreRegistriesResponse);
     }
 
 }
